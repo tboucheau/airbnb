26147ad657e2a1d319e136b97d7d79d4df68ba82
TonyRails <opposite240@gmail.com>
2017-07-12 18:02:46 +0000

    Création page utilisateur

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :path => '',
                      :path_names => { :sign_in => 'login', :sign_out => 'logout', :edit => 'profil' }
   root 'pages#home'
+  
+  resources :users, only: [ :show ]
 end
