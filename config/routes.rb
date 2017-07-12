1e4a91dcc7eac89eec050288d6c11b56966a0f5b
TonyRails <opposite240@gmail.com>
2017-07-12 18:18:34 +0000

    Page modification de profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '',
-                     :path_names => { :sign_in => 'login', :sign_out => 'logout', :edit => 'profil' }
+                     :path_names => { :sign_in => 'login', :sign_out => 'logout', :edit => 'profil' },
+                     :controllers => { :registrations => 'registrations' }
   root 'pages#home'
   
   resources :users, only: [ :show ]
