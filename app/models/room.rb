7f437be10d3e6b87fb1b6e0404880ae4d70609e8
TonyRails <opposite240@gmail.com>
2017-07-13 19:38:40 +0000

    Création du controller Room

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,5 +8,5 @@ class Room < ActiveRecord::Base
   validates :listing_name, presence: true, length: { maximum: 75 }
   validates :summary, presence: true, length: { maximum: 600 }
   validates :address, presence: true
-  validates :price, numericality: { only_integer: true, greater_than: 5 }, presence: true
+  validates :price, numericality: { only_integer: true, greater_than: 5 }
 end
