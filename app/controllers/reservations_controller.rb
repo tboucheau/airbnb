705fb13d0a4ca071fdffaed5f10f7477cec7f226
TonyRails <opposite240@gmail.com>
2017-07-16 20:46:35 +0000

    Création page voyages

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,6 +24,10 @@ class ReservationsController < ApplicationController
         redirect_to @reservation.room, notice: "Votre réservation a été acceptée"
     end
     
+    def your_trips
+        @trips = current_user.reservations
+    end
+    
     private
     def reservation_params
         params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
