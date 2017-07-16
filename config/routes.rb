1a870d9d08d74702231e5cd9a5e1d985639d6f16
TonyRails <opposite240@gmail.com>
2017-07-16 17:35:47 +0000

    Création model Reservations

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   
   resources :users, only: [ :show ]
-  resources :rooms
+  resources :rooms do
+    resources :reservations, only: [ :create ]
+  end
   resources :photos
+
 end
