67be087e9c51e6d348550891f561ec4fa8058fc7
TonyRails <opposite240@gmail.com>
2017-07-18 22:56:43 +0000

    Envoi de mail via SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ Rails.application.configure do
     :storage => :cloudinary,
     :path => ':id/:style/:filename'
   })
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'aiirbnbt.herokuapp.com', :protocol => 'https' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
